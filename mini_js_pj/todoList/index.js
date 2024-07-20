70b720b9d14679fff90762f006cbf34800655329
Kazu <hlyanw@yahoo.com>
2024-07-20 21:30:29 +0630

    Finished todoList

--- mini_js_pj/todoList/index.js
+++ mini_js_pj/todoList/index.js
@@ -7,25 +7,59 @@ remove items
 */
 
 let items = [];
+const stroageKey = "item";
 
-const itemDiv = document.getElementById("items");
+const itemsDiv = document.getElementById("items");
 
-function saveItems() {
+function renderItems() {
+    itemsDiv.innerHTML = null;
 
+    for (const [idx, item] of Object.entries(items)) {
+        const container = document.createElement("div");
+        container.style.marginBottom = "10px";
+
+        const text = document.createElement("p");
+        text.textContent = item;
+
+        const button = document.createElement("button");
+        button.textContent = "Delete";
+        button.onclick = () => removeItems(idx)
+
+        container.appendChild(text);
+        container.appendChild(button);
+
+        itemsDiv.appendChild(container);
+    }
 }
 
 function loadItems() {
-
+    const oldItems = localStorage.getItem(stroageKey);
+    if (oldItems) items = JSON.parse(oldItems);
+    renderItems();
 }
 
-function renderItems() {
-
+function saveItems() {
+    const stringItems = JSON.stringify(items);
+    localStorage.setItem(stroageKey, stringItems);
 }
 
 function addItems() {
-
+    const input = document.getElementById("itemInput");
+    const task = input.value;
+    if (!task) {
+        alert("You cannot add an empty item");
+        return;
+    }
+    items.push(task);
+    renderItems();
+    input.value = "";
+    saveItems();
 }
 
-function removeItems() {
-
+function removeItems(idx) {
+    items.splice(idx, 1);
+    renderItems();
+    saveItems();
 }
+
+document.addEventListener("DOMContentLoaded", loadItems)
